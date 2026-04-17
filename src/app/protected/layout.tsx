2e312d1dc4b4494de829cd84026cda33a23850c9
NapatrSaengthongsakullert <napatr.se@ku.th>
2026-04-17 14:35:44 +0700

    chore: refactor protected layout for better structure and maintainability.

--- src/app/protected/layout.tsx
+++ src/app/protected/layout.tsx
@@ -1,13 +1,8 @@
-import { createClient, ensureUserProfile } from "@/lib/supabase/server";
-
-export default async function ProtectedLayout({
+export default function ProtectedLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const supabase = await createClient();
-  await ensureUserProfile(supabase);
-
   return (
     <main className="min-h-screen w-full">
       <div className="flex min-h-screen w-full flex-col">
